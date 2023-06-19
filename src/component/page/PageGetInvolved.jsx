1402d7b7eab7f439dd86668dde07726bfd9c65cc
Alexis <alexis.prunier@securitymadein.lu>
2023-06-19 14:48:39 +0200

    Fill empty pages #2

--- src/component/page/PageGetInvolved.jsx
+++ src/component/page/PageGetInvolved.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./PageGetInvolved.css";
+import ComingSoon from "../box/ComingSoon.jsx";
 
 export default class PageGetInvolved extends React.Component {
 	constructor(props) {
@@ -13,6 +14,9 @@ export default class PageGetInvolved extends React.Component {
 	render() {
 		return (
 			<div id={"PageGetInvolved"}>
+				<ComingSoon
+					height={500}
+				/>
 			</div>
 		);
 	}
