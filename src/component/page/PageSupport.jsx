31fcf6cfe487ec2744e5fdf1fb74725b117c1d90
Alexis <alexis.prunier@securitymadein.lu>
2023-06-20 12:26:48 +0200

    Progress on support page

--- src/component/page/PageSupport.jsx
+++ src/component/page/PageSupport.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./PageSupport.css";
-import ComingSoon from "../box/ComingSoon.jsx";
 
 export default class PageSupport extends React.Component {
 	constructor(props) {
@@ -14,9 +13,61 @@ export default class PageSupport extends React.Component {
 	render() {
 		return (
 			<div id={"PageSupport"}>
-				<ComingSoon
-					height={500}
-				/>
+				<div className="max-sized-section">
+					<div className="row">
+						<div className="col-md-12">
+							<div className="immediate-support">
+								<div className="row">
+									<div className="col-md-12">
+										<h2>Immediat support</h2>
+									</div>
+
+									<div className="offset-md-2 col-md-8">
+										<p>Either you need help, seek guidance or wish
+										to discuss a cybersecurity related topic/project, relevant
+										experts are there for you.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="col-md-12">
+							<div className="report">
+								<div className="row">
+									<div className="col-md-1"/>
+
+									<div className="col-md-5">
+										<h4>Report an incident!</h4>
+
+										<p>Incidents can be reported via e-mail or phone. See
+										our contact page for details including OpenPGP
+										information.</p>
+
+										<button
+											className="link"
+											onClick={() => window.open(
+												"https://circl.lu",
+												"_blank",
+											)}>
+											Report an incident &nbsp;<i className="fas fa-arrow-right"/>
+										</button>
+									</div>
+
+									<div className="col-md-1"/>
+
+									<div className="col-md-3">
+										<div className="report-img">
+											<img
+												src="/img/circl-logo-full-text.png"
+												alt="CIRCL Logo"
+											/>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
