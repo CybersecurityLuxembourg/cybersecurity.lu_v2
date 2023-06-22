bd8cee5d21a3fe96183a4027055420e2f43782f6
Alexis <alexis.prunier@securitymadein.lu>
2023-06-22 13:27:22 +0200

    Add content to contact page

--- src/component/page/PageContact.jsx
+++ src/component/page/PageContact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./PageContact.css";
-import ComingSoon from "../box/ComingSoon.jsx";
+import BoxWithTitle from "../box/BoxWithTitle.jsx";
 
 export default class PageContact extends React.Component {
 	constructor(props) {
@@ -14,9 +14,42 @@ export default class PageContact extends React.Component {
 	render() {
 		return (
 			<div id="PageContact">
-				<ComingSoon
-					height={500}
-				/>
+				<div className="max-sized-section">
+					<div className="row">
+						<div className="offset-md-2 col-md-4">
+							<BoxWithTitle
+								title={"Email address"}
+								content={<div className="row">
+									<div className="col-md-12">
+										<div>info@cybersecurity-luxembourg.com</div>
+									</div>
+								</div>}
+							/>
+						</div>
+						<div className="col-md-4">
+							<BoxWithTitle
+								title={"Phone"}
+								content={<div className="row">
+									<div className="col-md-12">
+										<div>(+352) 274 00 98 601</div>
+									</div>
+								</div>}
+							/>
+						</div>
+						<div className="offset-md-4 col-md-4">
+							<BoxWithTitle
+								title={"Physical address"}
+								content={<div className="row">
+									<div className="col-md-12">
+										<div>Luxembourg House of Cybersecurity</div>
+										<div>122 rue Adolphe Fischer</div>
+										<div>L-1521 Luxembourg</div>
+									</div>
+								</div>}
+							/>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
