8306f4d5266f8833ec63fcfd9109f48ecdfda805
Alexis <alexis.prunier@securitymadein.lu>
2023-07-31 13:14:31 +0200

    Implement first version of the "Publications" page

---
+++ src/component/item/Resource.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import "./Resource.css";
+import { Link } from "react-router-dom";
+import dompurify from "dompurify";
+import CardSocialMedia from "./CardSocialMedia.jsx";
+
+export default class Resource extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+		};
+	}
+
+	getBoxContent() {
+		return <div>
+			<div className="card-body">
+				{this.props.icon
+					&& <div className="card-icon">
+						<i className={this.props.icon + " " + this.props.iconColor}/>
+					</div>
+				}
+				<div className="card-title">
+					{this.props.info.title}
+				</div>
+
+				<div className="card-text">
+					<div dangerouslySetInnerHTML={{
+						__html:
+						dompurify.sanitize(this.props.info.abstract),
+					}} />
+				</div>
+
+				<div className="row">
+					<div className="col-md-6">
+						<CardSocialMedia
+							article={this.props.info}
+						/>
+					</div>
+					<div className="col-md-6">
+						<button className={"link small"}>
+							Read more &nbsp;<i className="fas fa-arrow-right"/>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>;
+	}
+
+	render() {
+		return this.props.info.link
+			&& this.props.info.link.length > 0
+			? <div className="Resource card">
+				<a
+					href={this.props.info.link}
+					target={"_blank"}
+					rel="noreferrer"
+					className="link">
+					{this.getBoxContent()}
+				</a>
+			</div>
+			: <div className="Resource card">
+				<Link
+					to={"/resource/" + this.props.info.handle}
+					className="link">
+					{this.getBoxContent()}
+				</Link>
+			</div>;
+	}
+}
--- src/component/page/PagePublications.jsx
+++ src/component/page/PagePublications.jsx
@@ -1,22 +1,241 @@
 import React from "react";
 import "./PagePublications.css";
-import ComingSoon from "../box/ComingSoon.jsx";
+import { NotificationManager as nm } from "react-notifications";
+import { Link } from "react-router-dom";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+import { getUrlParameter, dictToURI } from "../../utils/url.jsx";
+import { getRequest } from "../../utils/request.jsx";
+import Resource from "../item/Resource.jsx";
+import Count from "../form/Count.jsx";
+import Field from "../form/Field.jsx";
+import Loading from "../box/Loading.jsx";
+import Message from "../box/Message.jsx";
+import DynamicTable from "../table/DynamicTable.jsx";
 
 export default class PagePublications extends React.Component {
 	constructor(props) {
 		super(props);
 
+		const initFilters = {
+			title: getUrlParameter("title"),
+			taxonomy_values: getUrlParameter("taxonomy_values")
+				? getUrlParameter("taxonomy_values").split(",") : [],
+		};
+
 		this.state = {
+			articles: null,
+			initFilters,
+			filters: initFilters,
 		};
 	}
 
-	// eslint-disable-next-line class-methods-use-this
+	componentDidMount() {
+		this.fetchArticles();
+	}
+
+	componentDidUpdate(_, prevState) {
+		if (JSON.stringify(prevState.filters) !== JSON.stringify(this.state.filters)) {
+			this.fetchArticles();
+		}
+	}
+
+	fetchArticles(page) {
+		const params = {
+			type: "RESOURCE",
+			page: page || 1,
+			per_page: 6,
+			include_tags: true,
+			...this.state.filters,
+		};
+
+		getRequest.call(this, "public/get_public_articles?"
+			+ dictToURI(params), (data) => {
+			this.setState({
+				articles: data,
+			});
+		}, (response) => {
+			nm.warning(response.statusText);
+		}, (error) => {
+			nm.error(error.message);
+		});
+	}
+
+	buildArticleList() {
+		if (!this.state.articles) {
+			return <Loading height={500}/>;
+		}
+
+		if (this.state.articles.pagination.total === 0) {
+			return <Message height={500} text={"No item found"}/>;
+		}
+
+		return <DynamicTable
+			items={this.state.articles.items}
+			pagination={this.state.articles.pagination}
+			changePage={(page) => this.fetchArticles(page)}
+			buildElement={(a) => (
+				<div className="col-md-6">
+					<Resource
+						info={a}
+					/>
+				</div>
+			)}
+		/>;
+	}
+
+	buildClassificationFilters() {
+		if (!this.props.taxonomies) {
+			return <Loading/>;
+		}
+
+		const result = [];
+
+		const legalCategories = this.props.taxonomies.taxonomy_values
+			.filter((c) => c.category === "RESOURCE CATEGORY")
+			.sort((a, b) => a.name - b.name);
+
+		for (let i = 0; i < legalCategories.length; i++) {
+			result.push(
+				<div>
+					<Field
+						className="checkbox-category"
+						type="checkbox"
+						checkBoxLabel={<b>{legalCategories[i].name}</b>}
+						value={this.state.filters.taxonomy_values.indexOf(legalCategories[i].id) >= 0}
+						onChange={(v) => this.modifyFilters(
+							"taxonomy_values",
+							v
+								? this.state.filters.taxonomy_values.concat([legalCategories[i].id])
+								: this.state.filters.taxonomy_values.filter((o) => o !== legalCategories[i].id),
+						)}
+						hideLabel={true}
+					/>
+				</div>,
+			);
+		}
+
+		return result;
+	}
+
+	modifyFilters(field, value) {
+		const filters = { ...this.state.filters };
+		filters[field] = value;
+		this.setState({ filters });
+	}
+
+	clearFilters() {
+		this.setState({ filters: this.state.initFilters });
+	}
+
 	render() {
 		return (
-			<div id={"PagePublications"}>
-				<ComingSoon
-					height={500}
-				/>
+			<div id="PagePublications">
+				<div className="top-content">
+					<div className="max-sized-section">
+						<div className="row">
+							<div className="col-md-12">
+								<Breadcrumb>
+									<Breadcrumb.Item>RESOURCES & SUPPORT</Breadcrumb.Item>
+									<Breadcrumb.Item><Link to="/publications">Publications</Link></Breadcrumb.Item>
+								</Breadcrumb>
+							</div>
+
+							<div className="col-md-12">
+								<div className="text-content">
+									<div className="h8 blue-text uppercase">Publications</div>
+								</div>
+							</div>
+
+							<div className="col-md-5">
+								<div className="text-content">
+									<h4>Text to be defined</h4>
+								</div>
+							</div>
+
+							<div className="col-md-1"/>
+
+							<div className="col-md-5">
+								<p>Text to be defined</p>
+
+								<p>Text to be defined</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="max-sized-section">
+					<div className="list-content">
+						<div className="row">
+							<div className="col-md-8">
+								<div className="vertically-centered">
+									<h6>Publications</h6>
+								</div>
+							</div>
+
+							<div className="col-md-4">
+								<Field
+									hideLabel={true}
+									placeholder="Search"
+									value={this.state.filters.title}
+									onChange={(v) => this.modifyFilters("title", v)}
+								/>
+							</div>
+
+							<div className="col-md-12">
+								<div className="grey-horizontal-bar list-separator"/>
+							</div>
+						</div>
+
+						<div className="row">
+							<div className="col-md-3">
+								<div className="box filter-box">
+									<div className="row">
+										<div className="col-md-6">
+											<h6 className="blue-text">Filter by</h6>
+										</div>
+
+										<div className="col-md-6">
+											<div className="right-buttons">
+												<button
+													className="link small"
+													onClick={() => this.clearFilters()}>
+													Clear all
+												</button>
+											</div>
+										</div>
+
+										<div className="col-md-12">
+											<div className="grey-horizontal-bar"/>
+
+											<div className="h8">
+												CATEGORY
+											</div>
+										</div>
+
+										<div className="col-md-12">
+											{this.buildClassificationFilters()}
+										</div>
+									</div>
+								</div>
+							</div>
+
+							<div className="col-md-1"/>
+
+							<div className="col-md-8">
+								<span className="h8">Publications</span>
+
+								{this.state.articles
+									&& this.state.articles.pagination.total > 0
+									&& <Count count={this.state.articles.pagination.total}/>
+								}
+
+								<div className="row">
+									{this.buildArticleList()}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
