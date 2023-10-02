7e301e8baccbd3cdc6f04a4b89b3ab1642284479
Alexis <alexis.prunier@securitymadein.lu>
2023-10-02 16:23:52 +0200

    Start launch pad section of startup implementation

--- src/component/page/pagesupportforstartups/PageSupportForStartupsLaunchpad.jsx
+++ src/component/page/pagesupportforstartups/PageSupportForStartupsLaunchpad.jsx
@@ -7,6 +7,335 @@ export default class PageSupportForStartupsLaunchpad extends React.Component {
 		return (
 			<div id="PageSupportForStartupsLaunchpad">
 				<div className="max-sized-section">
+					<div className="row spaced-row">
+						<div className="col-md-12 centered">
+							<h4>Luxembourg’s launchpad for your cyber business</h4>
+						</div>
+
+						<div className="col-md-8 offset-md-2">
+							<h6>Why choosing Luxembourg?</h6>
+						</div>
+
+						<div className="col-md-4 offset-md-2">
+							<div className="box vertically-centered centered">
+								<div className="row">
+									<div className="col-md-6 offset-md-3 spaced-row">
+										<i className="fas fa-mountain"/>
+									</div>
+
+									<div className="col-md-8 offset-md-2">
+										<div className="h8 spaced-row">
+											At a glance
+										</div>
+									</div>
+
+									<div className="col-md-12">
+										<button
+											className="small">
+											View the content
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="col-md-4">
+							<div className="box vertically-centered centered">
+								<div className="row">
+									<div className="col-md-6 offset-md-3 spaced-row">
+										<i className="fas fa-tasks"/>
+									</div>
+
+									<div className="col-md-8 offset-md-2">
+										<div className="h8 spaced-row">
+											10 good reasons to choose Luxembourg
+										</div>
+									</div>
+
+									<div className="col-md-12">
+										<button
+											className="small">
+											Download the brochure
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div className="row spaced-row">
+						<div className="col-md-12 centered">
+							<h4>Ready for take-off</h4>
+						</div>
+
+						<div className="col-md-4">
+							<div className="title">
+								<h6>Accelerators & Incubators</h6>
+							</div>
+						</div>
+
+						<div className="col-md-4">
+							<div className="box vertically-centered centered">
+								<div className="row">
+									<div className="col-md-6 offset-md-3 spaced-row">
+										<i className="fas fa-rocket"/>
+									</div>
+
+									<div className="col-md-8 offset-md-2">
+										<div className="h8 spaced-row">
+											Accelerators & Incubators
+										</div>
+									</div>
+
+									<div className="col-md-12">
+										<button
+											className="small">
+											Open the list
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div className="row spaced-row">
+						<div className="col-md-4">
+							<div className="title">
+								<h6>Investors</h6>
+							</div>
+						</div>
+
+						<div className="col-md-4">
+							<div className="box vertically-centered centered">
+								<div className="row">
+									<div className="col-md-6 offset-md-3 spaced-row">
+										<i className="fas fa-money-check-alt"/>
+									</div>
+
+									<div className="col-md-8 offset-md-2">
+										<div className="h8 spaced-row">
+											Venture Capitals
+										</div>
+									</div>
+
+									<div className="col-md-12">
+										<button
+											className="small">
+											Open the list
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="col-md-4">
+							<div className="box vertically-centered centered">
+								<div className="row">
+									<div className="col-md-6 offset-md-3 spaced-row">
+										<i className="fas fa-handshake"/>
+									</div>
+
+									<div className="col-md-8 offset-md-2">
+										<div className="h8 spaced-row">
+											Business Angels
+										</div>
+									</div>
+
+									<div className="col-md-12">
+										<button
+											className="small">
+											Open the list
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div className="row spaced-row">
+						<div className="col-md-4">
+							<div className="title">
+								<h6>Financial aid</h6>
+							</div>
+						</div>
+
+						<div className="col-md-4">
+							<div className="box vertically-centered centered">
+								<div className="row">
+									<div className="col-md-6 offset-md-3 spaced-row">
+										<i className="fas fa-money-check-alt"/>
+									</div>
+
+									<div className="col-md-8 offset-md-2">
+										<div className="h8 spaced-row">
+											Venture Capitals
+										</div>
+									</div>
+
+									<div className="col-md-12">
+										<button
+											className="small">
+											Open the list
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="col-md-4">
+							<div className="box vertically-centered centered">
+								<div className="row">
+									<div className="col-md-6 offset-md-3 spaced-row">
+										<i className="fas fa-handshake"/>
+									</div>
+
+									<div className="col-md-8 offset-md-2">
+										<div className="h8 spaced-row">
+											Business Angels
+										</div>
+									</div>
+
+									<div className="col-md-12">
+										<button
+											className="small">
+											Open the list
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div className="row spaced-row">
+						<div className="col-md-4">
+							<div className="title">
+								<h6>Support programmes</h6>
+							</div>
+						</div>
+
+						<div className="col-md-4">
+							<div className="box vertically-centered centered">
+								<div className="row">
+									<div className="col-md-6 offset-md-3 spaced-row">
+										<i className="fas fa-money-check-alt"/>
+									</div>
+
+									<div className="col-md-8 offset-md-2">
+										<div className="h8 spaced-row">
+											Venture Capitals
+										</div>
+									</div>
+
+									<div className="col-md-12">
+										<button
+											className="small">
+											Open the list
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="col-md-4">
+							<div className="box vertically-centered centered">
+								<div className="row">
+									<div className="col-md-6 offset-md-3 spaced-row">
+										<i className="fas fa-handshake"/>
+									</div>
+
+									<div className="col-md-8 offset-md-2">
+										<div className="h8 spaced-row">
+											Business Angels
+										</div>
+									</div>
+
+									<div className="col-md-12">
+										<button
+											className="small">
+											Open the list
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="col-md-4 offset-md-4">
+							<div className="box vertically-centered centered">
+								<div className="row">
+									<div className="col-md-6 offset-md-3 spaced-row">
+										<i className="fas fa-handshake"/>
+									</div>
+
+									<div className="col-md-8 offset-md-2">
+										<div className="h8 spaced-row">
+											Business Angels
+										</div>
+									</div>
+
+									<div className="col-md-12">
+										<button
+											className="small">
+											Open the list
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div className="row spaced-row">
+						<div className="col-md-4">
+							<div className="title">
+								<h6>Competitions</h6>
+							</div>
+						</div>
+
+						<div className="col-md-4">
+							<div className="box vertically-centered centered">
+								<div className="row">
+									<div className="col-md-6 offset-md-3 spaced-row">
+										<i className="fas fa-money-check-alt"/>
+									</div>
+
+									<div className="col-md-8 offset-md-2">
+										<div className="h8 spaced-row">
+											Venture Capitals
+										</div>
+									</div>
+
+									<div className="col-md-12">
+										<button
+											className="small">
+											Open the list
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div className="col-md-4">
+							<div className="box vertically-centered centered">
+								<div className="row">
+									<div className="col-md-6 offset-md-3 spaced-row">
+										<i className="fas fa-handshake"/>
+									</div>
+
+									<div className="col-md-8 offset-md-2">
+										<div className="h8 spaced-row">
+											Business Angels
+										</div>
+									</div>
+
+									<div className="col-md-12">
+										<button
+											className="small">
+											Open the list
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
