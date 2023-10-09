33e45d442a49767262ceef98e9bf17fe315e58cc
Alexis <alexis.prunier@securitymadein.lu>
2023-10-09 10:24:09 +0200

    Integrate external redirection for documents

--- src/component/InsideApp.jsx
+++ src/component/InsideApp.jsx
@@ -220,9 +220,20 @@ class InsideApp extends React.Component {
 							{...props}
 						/>}/>
 
-						{/* Redirections */}
+						{/* Internal redirections */}
 
 						<Redirect from="/company/:id" to="/entity/:id" />
+
+						{/* External redirections */}
+
+						<Route
+							path="/luxchat/privacy-policy"
+							component={() => { window.location = "https://api.cybersecurity.lu/public/get_public_document/Luxchat - privacy policy.pdf"; return null; } }
+						/>
+						<Route
+							path="/luxchat/general-terms-and-condition"
+							component={() => { window.location = "https://api.cybersecurity.lu/public/get_public_document/Luxchat%20-%20general%20terms%20and%20conditions.pdf"; return null; } }
+						/>
 
 						{/* 404 */}
 
