e19b5f427bf1fa51995dee22cc24edd5e858c314
Alexis <alexis.prunier@securitymadein.lu>
2024-01-23 14:18:31 +0100

    Fix closing button on popup box

--- src/component/box/PopupNews.jsx
+++ src/component/box/PopupNews.jsx
@@ -6,40 +6,45 @@ export default class PopupNews extends React.Component {
 		super(props);
 
 		this.state = {
+			show: true,
 		};
 	}
 
 	render() {
-		return (
-			<div className={"PopupNews"}>
-				<div className="row">
-					<div className="col-md-10">
-						<div className="h8"><i className="fas fa-tv"/>&nbsp;ICRC Symposium</div>
-					</div>
-					<div className="col-md-12 mt-3">
-						Follow the livestream of the Opening & Closing sessions of the ICRC Symposium here
-					</div>
-					<div className="col-md-12">
-						<div className="right-buttons">
-							<button
-								className="link small"
-								onClick={() => window.open(
-									"https://www.cybersecurity.lu/news/follow-the-livestream-of-the-international-red-cross-committee-symposium",
-									"_blank",
-								)}>
-								Go to the livestream &nbsp;<i className="fas fa-arrow-right"/>
-							</button>
+		if (this.state.show) {
+			return (
+				<div className={"PopupNews"}>
+					<div className="row">
+						<div className="col-md-10">
+							<div className="h8"><i className="fas fa-tv"/>&nbsp;ICRC Symposium</div>
+						</div>
+						<div className="col-md-12 mt-3">
+							Follow the livestream of the Opening & Closing sessions of the ICRC Symposium here
+						</div>
+						<div className="col-md-12">
+							<div className="right-buttons">
+								<button
+									className="link small"
+									onClick={() => window.open(
+										"https://www.cybersecurity.lu/news/follow-the-livestream-of-the-international-red-cross-committee-symposium",
+										"_blank",
+									)}>
+									Go to the livestream &nbsp;<i className="fas fa-arrow-right"/>
+								</button>
+							</div>
 						</div>
 					</div>
-				</div>
 
-				<div className="close-button">
-					<i
-						className="fas fa-times CheckBox-icon"
-						onClick={() => this.setState({ show: false })}
-					/>
+					<div className="close-button">
+						<i
+							className="fas fa-times CheckBox-icon"
+							onClick={() => this.setState({ show: false })}
+						/>
+					</div>
 				</div>
-			</div>
-		);
+			);
+		}
+
+		return "";
 	}
 }
