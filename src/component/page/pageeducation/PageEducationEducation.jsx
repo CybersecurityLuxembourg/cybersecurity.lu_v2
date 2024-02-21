1db4c762b2e1d452c8d13555b719e8e657d57c66
Ruslan Baidan <ruslan.baidan@nc3.lu>
2024-02-21 14:32:31 +0100

    CS fixes.

--- src/component/page/pageeducation/PageEducationEducation.jsx
+++ src/component/page/pageeducation/PageEducationEducation.jsx
@@ -9,124 +9,124 @@ import Message from "../../box/Message.jsx";
 import Loading from "../../box/Loading.jsx";
 
 export default class PageEducationEducation extends React.Component {
-		constructor(props) {
-				super(props);
+	constructor(props) {
+		super(props);
 
-				this.state = {
-						educationEntities: null,
-						educationServices: null,
-				};
+		this.state = {
+			educationEntities: null,
+			educationServices: null,
+		};
+	}
+
+	componentDidMount() {
+		this.fetchEducationEntities();
+	}
+
+	componentDidUpdate(prevProps) {
+		if (!prevProps.taxonomies && this.props.taxonomies) {
+			this.fetchEducationEntities();
 		}
+	}
 
-		componentDidMount() {
-				this.fetchEducationEntities();
-		}
+	fetchEducationEntities() {
+		if (this.props.taxonomies) {
+			const valueId = this.props.taxonomies.taxonomy_values
+				.filter((v) => v.category === "ECOSYSTEM ROLE" && v.name === "EDUCATIONAL INSTITUTION")
+				.map((v) => v.id);
 
-		componentDidUpdate(prevProps) {
-				if (!prevProps.taxonomies && this.props.taxonomies) {
-						this.fetchEducationEntities();
-				}
-		}
-
-		fetchEducationEntities() {
-				if (this.props.taxonomies) {
-						const valueId = this.props.taxonomies.taxonomy_values
-								.filter((v) => v.category === "ECOSYSTEM ROLE" && v.name === "EDUCATIONAL INSTITUTION")
-								.map((v) => v.id);
-
-						if (valueId.length > 0) {
-								this.setState({
-										educationEntities: null,
-								}, () => {
-										getRequest.call(this, "public/get_public_entities"
-												+ "?taxonomy_values=" + valueId.join(","), (data) => {
-												this.setState({
-														educationEntities: data,
-												}, () => {
-														this.fetchServicesOfEducationEntities();
-												});
-										}, (response) => {
-												nm.warning(response.statusText);
-										}, (error) => {
-												nm.error(error.message);
-										});
-								});
-						} else {
-								this.setState({
-										educationEntities: [],
-								});
-						}
-				}
-		}
-
-		fetchServicesOfEducationEntities() {
-				if (this.state.educationEntities && this.state.educationEntities.length > 0) {
-						const params = {
-								type: "SERVICE",
-								include_tags: true,
-								entities: this.state.educationEntities.map((e) => (e.id)),
-						};
-
-						getRequest.call(this, "public/get_public_articles?"
-								+ dictToURI(params), (data) => {
-								this.setState({
-										educationServices: data,
-								});
-						}, (response) => {
-								nm.warning(response.statusText);
-						}, (error) => {
-								nm.error(error.message);
-						});
-				} else {
+			if (valueId.length > 0) {
+				this.setState({
+					educationEntities: null,
+				}, () => {
+					getRequest.call(this, "public/get_public_entities"
+						+ "?taxonomy_values=" + valueId.join(","), (data) => {
 						this.setState({
-								educationServices: { pagination: { total: 0 } },
+							educationEntities: data,
+						}, () => {
+							this.fetchServicesOfEducationEntities();
 						});
-				}
+					}, (response) => {
+						nm.warning(response.statusText);
+					}, (error) => {
+						nm.error(error.message);
+					});
+				});
+			} else {
+				this.setState({
+					educationEntities: [],
+				});
+			}
+		}
+	}
+
+	fetchServicesOfEducationEntities() {
+		if (this.state.educationEntities && this.state.educationEntities.length > 0) {
+			const params = {
+				type: "SERVICE",
+				include_tags: true,
+				entities: this.state.educationEntities.map((e) => (e.id)),
+			};
+
+			getRequest.call(this, "public/get_public_articles?"
+				+ dictToURI(params), (data) => {
+				this.setState({
+					educationServices: data,
+				});
+			}, (response) => {
+				nm.warning(response.statusText);
+			}, (error) => {
+				nm.error(error.message);
+			});
+		} else {
+			this.setState({
+				educationServices: { pagination: { total: 0 } },
+			});
+		}
+	}
+
+	buildContent() {
+		if (!this.state.educationEntities || !this.state.educationServices) {
+			return <Loading height={500}/>;
 		}
 
-		buildContent() {
-				if (!this.state.educationEntities || !this.state.educationServices) {
-						return <Loading height={500}/>;
-				}
-
-				if (this.state.educationEntities.length === 0) {
-						return <Message height={500} text={"No message found"}/>;
-				}
-
-				return <div>
-						{this.state.educationEntities.map((e) => (
-								<div className="education-section" key={e.id}>
-										<div className="row">
-												<div className="col-md-4">
-														<Entity info={e}/>
-												</div>
-
-												<div className="col-md-8">
-														<div className="row">
-																{this.state.educationServices.items
-																		.filter((s) => s.entity_tags.indexOf(e.id) >= 0)
-																		.map((s) => (
-																				<div className="col-md-6" key={s.id}>
-																						<Service
-																								info={s}
-																								taxnomies={this.props.taxnomies}
-																						/>
-																				</div>
-																		))}
-														</div>
-												</div>
-										</div>
-								</div>
-						))}
-
-				</div>;
+		if (this.state.educationEntities.length === 0) {
+			return <Message height={500} text={"No message found"}/>;
 		}
 
-		render() {
-				return (
-						<div id="PageEducationEducation">
-								{this.buildContent()}
+		return <div>
+			{this.state.educationEntities.map((e) => (
+				<div className="education-section" key={e.id}>
+					<div className="row">
+						<div className="col-md-4">
+							<Entity info={e}/>
 						</div>
-				);
-		}
+
+						<div className="col-md-8">
+							<div className="row">
+								{this.state.educationServices.items
+									.filter((s) => s.entity_tags.indexOf(e.id) >= 0)
+									.map((s) => (
+										<div className="col-md-6" key={s.id}>
+											<Service
+												info={s}
+												taxnomies={this.props.taxnomies}
+											/>
+										</div>
+									))}
+							</div>
+						</div>
+					</div>
+				</div>
+			))}
+
+		</div>;
+	}
+
+	render() {
+		return (
+			<div id="PageEducationEducation">
+				{this.buildContent()}
+			</div>
+		);
+	}
 }
