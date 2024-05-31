93a4532471ce91ce41e1d43c6f81f0c0d22b12fc
Philippe Parage <philippe.parage@nc3.lu>
2024-05-31 09:08:14 +0200

    Button Design fix

--- src/component/bar/Menu.jsx
+++ src/component/bar/Menu.jsx
@@ -305,11 +305,10 @@ export default class Menu extends React.Component {
 
 							<div className="separator"/>
 							<div className="split-bar"/>
-
-							<a className="red small immediate"
-								href="/support"
-							>
-								IMMEDIATE SUPPORT
+							<a href="/support">
+								<button className="red small">
+									IMMEDIATE SUPPORT
+								</button>
 							</a>
 						</Nav>
 					</Navbar.Collapse>
